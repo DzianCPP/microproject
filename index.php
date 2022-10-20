93202d589035b1c59e336b55ea8d858983a30c19
DzianCPP <ocherkiprozaika@gmail.com>
2022-10-20 10:09:28 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>MicroProject</title>
+</head>
+
+<body>
+    <div class="mainDiv">
+        <h1>
+            Insert Your Name
+        </h1>
+
+        <br>
+
+        <input type="text" name="insertName" id="insertName" placeholder="insert name here"><br>
+
+        <input type="submit" name="btnSubmit" id="btnSubmot" value="Submit"><br>
+
+    </div>
+</body>
+
+</html>
